530371c9aa25b3f12122090e2331b97a9a6aeb65
WilsonJereKlein <mic.galarce@duocuc.cl>
2025-04-09 18:06:23 -0400

    clima.py

--- clima.py
+++ clima.py
@@ -1,2 +1,15 @@
-Hola
-hola 2
+import json
+
+with open(r'D:\Michael\Script\clima.json', 'r') as json_file:
+    datos = json.load(json_file)
+
+print("=== MENÚ CLIMA ===")
+print("1) Ver ciudades disponibles") 
+print("2) Consultar clima por ciudad") 
+print("3) Salir")
+opcion = input("Seleccione una opción (1-3): ")
+
+ciudad_list = []
+for pais in datos['ciudades']:
+    for nombre in datos['ciudades']:
+    ciudad_list.append({"ciudad": nombre[nombre]})
